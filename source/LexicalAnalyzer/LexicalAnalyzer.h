99defe7f970771a55a4166962703d2ac3dde9d36
Tiger Sachse <tiger@sachse.io>
2018-06-07 15:28:52 -0400

    beginning project

---
+++ source/LexicalAnalyzer/LexicalAnalyzer.h
@@ -0,0 +1,18 @@
+#define DEFAULT_OUTFILE "ff.out"
+
+enum {
+    NUL = 1, IDENTIFIER,
+    NUMBER, PLUS, MINUS,
+    MULTIPLY, SLASH, ODD,
+    EQUAL, NOT_EQUAL, LESS,
+    LESS_EQUAL, GREATER,
+    GREATER_EQUAL, LEFT_PARENTHESIS,
+    RIGHT_PARENTHESIS, COMMA,
+    SEMICOLON, PERIOD, BECOME,
+    BEGIN, END, IF, THEN, WHILE,
+    DO, CALL, CONSTANT, VARIABLE,
+    PROCEDURE, WRITE, READ, ELSE
+} TokenTypes;
+
+void skipComment(FILE*);
+int analyzeSource(char*, char*);
