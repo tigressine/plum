adb57643e04b344d9624ae50e3651e043470e409
Tiger Sachse <tiger@sachse.io>
2018-06-08 14:42:17 -0400

    changing naming conventions

---
+++ source/machine/machine.h
@@ -0,0 +1,126 @@
+#include <limits.h>
+
+#ifndef VIRTUALMACHINE_HEADER
+#define VIRTUALMACHINE_HEADER
+
+// Convenience definitions for PL/0 opcode keywords.
+#define LIT 1
+#define RTN 2
+#define LOD 3
+#define STO 4
+#define CAL 5
+#define INC 6
+#define JMP 7
+#define JPC 8
+#define SIO 9
+#define NEG 10
+#define ADD 11
+#define SUB 12
+#define MUL 13
+#define DIV 14
+#define ODD 15
+#define MOD 16
+#define EQL 17
+#define NEQ 18
+#define LSS 19
+#define LEQ 20
+#define GTR 21
+#define GEQ 22
+
+// Trace flag constants (defined in hexidecimal to be MLG).
+#define TRACE_CPU 0x1
+#define TRACE_RECORDS 0x2
+#define TRACE_REGISTERS 0x4
+
+// Constants.
+#define INT_OFFSET 4
+#define MAX_LINES 100
+#define REGISTER_COUNT 16
+#define OP_FAILURE INT_MIN
+#define OP_SUCCESS INT_MIN + 1
+#define KILL_PROGRAM INT_MIN + 2
+
+// Instruction struct for each line of PL/0 code.
+typedef struct instruction {
+    int opCode;
+    int RField;
+    int LField;
+    int MField;
+} instruction;
+
+// CPU struct to hold registers and the current instruction.
+typedef struct CPU {
+    int registers[REGISTER_COUNT];
+    int programCounter;
+    int instructionCount;
+    instruction instRegister;
+} CPU;
+
+// An item in an activation record stack. Also serves as a node in
+// a linked list (that's how the stack is implemented).
+typedef struct recordStackItem {
+    int *locals;
+    int localCount;
+    int returnValue;
+    int returnAddress;
+    struct recordStackItem *staticLink;
+    struct recordStackItem *dynamicLink;
+} recordStackItem;
+
+// Container struct for a record linked list struct. Also keeps track
+// of the number of nodes in the stack.
+typedef struct recordStack {
+    int records;
+    recordStackItem *currentRecord;
+} recordStack;
+
+// Processing functional prototypes.
+CPU *createCPU(int);
+int freeCPU(CPU*);
+int countInstructions(char*);
+instruction *loadInstructions(char*, int);
+int processInstructions(instruction*, int, int);
+int fetchInstruction(CPU*, instruction*);
+int executeInstruction(CPU*, recordStack*);
+int freeInstructions(instruction*);
+void printStackTraceHeader(int);
+void printStackTraceLine(CPU*, recordStack*, int);
+void printRegisters(CPU*);
+void printCPU(CPU*);
+void printRecords(recordStackItem*);
+
+// Operations functional prototypes.
+int opLiteral(CPU*);
+int opReturn(CPU*, recordStack*);
+int opLoad(CPU*, recordStack*);
+int opStore(CPU*, recordStack*);
+int opCall(CPU*, recordStack*);
+int opAllocate(CPU*, recordStack*);
+int opJump(CPU*);
+int opConditionalJump(CPU*);
+int opSystemCall(CPU*);
+int opNegate(CPU*);
+int opAdd(CPU*);
+int opSubtract(CPU*);
+int opMultiply(CPU*);
+int opDivide(CPU*);
+int opIsOdd(CPU*);
+int opModulus(CPU*);
+int opIsEqual(CPU*);
+int opIsNotEqual(CPU*);
+int opIsLessThan(CPU*);
+int opIsLessThanOrEqualTo(CPU*);
+int opIsGreaterThan(CPU*);
+int opIsGreaterThanOrEqualTo(CPU*);
+
+// Stack functional prototypes.
+recordStack *initializeRecordStack(void);
+int pushRecord(CPU*, recordStack*);
+int popRecord(recordStack*);
+recordStackItem *peekRecord(recordStack*);
+int allocateLocals(recordStackItem*, int);
+recordStackItem *getDynamicParent(recordStack*, int);
+recordStackItem *getStaticParent(recordStack*, int);
+int isEmpty(recordStack*);
+recordStack *destroyRecordStack(recordStack*);
+#endif
---
+++ source/machine/stack.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include "machine.h"
+
+// Return an empty record stack.
+recordStack *initializeRecordStack(void) {
+    return calloc(1, sizeof(recordStack));
+}
+
+// Push a new record onto the stack.
+int pushRecord(CPU *cpu, recordStack *stack) {
+    recordStackItem *newRecord;
+    
+    if (cpu == NULL || stack == NULL) {
+        return OP_FAILURE;
+    }
+
+    // If there is not enough memory for a new record, return OP_FAILURE.
+    if ((newRecord = malloc(sizeof(recordStackItem))) == NULL) {
+        return OP_FAILURE;
+    }
+
+    newRecord->locals = NULL;
+    newRecord->localCount = 0;
+    newRecord->returnValue = 0;
+    newRecord->returnAddress = cpu->programCounter;
+    newRecord->dynamicLink = stack->currentRecord;
+    if ((newRecord->staticLink = getStaticParent(stack, cpu->instRegister.LField)) == NULL) {
+        newRecord->staticLink = newRecord; 
+    }
+    
+    // Set the top of the stack to be newRecord.
+    stack->currentRecord = newRecord;
+    stack->records++;
+
+    return OP_SUCCESS;
+}
+
+// Remove the top record from the stack and free any associated dynamic memory.
+int popRecord(recordStack *stack) {
+    recordStackItem *nextRecord;
+    int returnValue;
+
+    if (stack == NULL || stack->currentRecord == NULL) {
+        return OP_FAILURE;
+    }
+
+    nextRecord = stack->currentRecord->dynamicLink;
+    returnValue = stack->currentRecord->returnValue;
+    free(stack->currentRecord->locals);
+    free(stack->currentRecord);
+   
+    // Set the top of the stack to the record below the old top.
+    stack->currentRecord = nextRecord;
+    stack->records--;
+
+    return returnValue;
+}
+
+// Return the top record of the stack.
+recordStackItem *peekRecord(recordStack *stack) {
+    return (stack == NULL) ? NULL : stack->currentRecord;
+}
+
+// Allocate the locals array in the given record.
+int allocateLocals(recordStackItem *record, int localCount) {
+    if (record == NULL || record->locals != NULL) {
+        return OP_FAILURE;
+    }
+
+    record->localCount = localCount;
+    
+    // Set locals to NULL if there aren't any parameters.
+    if (localCount < 1) {
+        record->locals = NULL;
+    }
+    // Else make a new locals array in the record.
+    else if ((record->locals = calloc(1, sizeof(int) * localCount)) == NULL) {
+        record->localCount = 0;
+        
+        return OP_FAILURE;
+    }
+    
+    return OP_SUCCESS;
+}
+
+// Seek down the stack through dynamic links (in order).
+recordStackItem *getDynamicParent(recordStack *stack, int levels) {
+    recordStackItem *desiredRecord;
+
+    if (stack == NULL) {
+        return NULL;
+    }
+    
+    desiredRecord = stack->currentRecord;
+  
+    // While there are still more levels to go and desiredRecord hasn't reached
+    // the bottom of the stack, go deeper.
+    while (levels > 0 && desiredRecord != NULL) {
+        desiredRecord = desiredRecord->dynamicLink;
+        levels--;
+    }
+
+    return desiredRecord;
+}
+
+// Seek down the stack through static links (in lexicographical order).
+recordStackItem *getStaticParent(recordStack *stack, int levels) {
+    recordStackItem *desiredRecord;
+
+    if (stack == NULL) {
+        return NULL;
+    }
+
+    desiredRecord = stack->currentRecord;
+    
+    // While there are still more levels to go and desiredRecord hasn't reached
+    // the bottom of the stack, go deeper.
+    while (levels > 0 && desiredRecord != NULL) {
+        desiredRecord = desiredRecord->staticLink;
+        levels--;
+    }
+
+    return desiredRecord;
+}
+
+// Return if stack is empty or not.
+int isEmpty(recordStack *stack) {
+    return (stack == NULL || stack->records == 0) ? 1 : 0;
+}
+
+// Destroy the record stack.
+recordStack *destroyRecordStack(recordStack *stack) {
+    // Pop all records out of the stack.
+    while (!isEmpty(stack)) {
+        popRecord(stack);
+    }
+
+    // Free the stack container.
+    free(stack);
+
+    return NULL;
+}
