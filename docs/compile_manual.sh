2c2117e688e5eb51396d2db8141667c90c70da7c
Tiger Sachse <tiger@sachse.io>
2018-07-23 00:18:08 -0400

    finished manual and wrote build script

--- docs/compile_manual.sh
+++ docs/compile_manual.sh
@@ -1,12 +1,20 @@
+# Compile the manual into a pdf and delete all the leftover garbage.
+
+# Change this variable to change the name of the input and result.
 STEM="MANUAL"
 
-#xelatex $STEM.tex
+# Compile the tex file.
+xelatex $STEM.tex
 
+# Check all files in the directory. There's probably a cleaner, more "shell"
+# way to do this. Shell is rough to write...
 for FILE in *
 do
+    # If the file is related to the manual, then check if it is the input
+    # or the output. If it's neither, eliminate it.
     if echo $FILE | grep -q $STEM
     then
-        if $FILE -neq $STEM.pdf && $FILE -neq $STEM.tex
+        if [ "$FILE" != "$STEM.pdf" ] && [ "$FILE" != "$STEM.tex" ]
         then
             rm $FILE
         fi
