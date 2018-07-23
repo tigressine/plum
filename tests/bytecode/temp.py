43ae6daacee12f35b2b71b2e8f41606d000bf2bc
Tiger Sachse <tiger@sachse.io>
2018-07-23 00:40:07 -0400

    remove tests

--- tests/bytecode/temp.py
+++
@@ -1,9 +0,0 @@
-import random
-
-with open("too_long.plc", "w") as f:
-    for i in range(2000):
-        for j in range(4):
-            f.write(str(random.randrange(100)))
-            if j < 3:
-                f.write(" ")
-        f.write("\n")
